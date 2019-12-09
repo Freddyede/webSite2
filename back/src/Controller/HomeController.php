dc560d8802595cc8d0abd792341e5071cf976b17
Franck <patouillardfranck3@gmail.com>
2019-12-09 17:28:20 +0100

    Edit for API json

--- back/src/Controller/HomeController.php
+++ back/src/Controller/HomeController.php
@@ -4,17 +4,24 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/", name="home")
+     * @Route("/", name="home", methods={"GET"})
      */
-    public function index()
+    public function index(SerializerInterface $serializer)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
+        $response = new Response(
+            json_encode(['message'=>'Content']),
+            Response::HTTP_OK,
+            [
+                'content-type' => 'application/json',
+                'Access-Control-Allow-Origin'=>'*'
+            ]
+        );
+        return $response;
     }
 }

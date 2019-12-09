4cdd7d7be06e08ee3143bdeb05e4fb6de61ff185
Franck <patouillardfranck3@gmail.com>
2019-12-09 18:12:36 +0100

    Delete ArrayCollection and Groups because it not use

--- back/src/Entity/Product.php
+++ back/src/Entity/Product.php
@@ -2,9 +2,7 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
-use Symfony\Component\Serializer\Annotation\Groups;
 use Doctrine\ORM\Mapping as ORM;
 
 /**

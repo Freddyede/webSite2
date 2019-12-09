ca95ce774cd58a011527668a6d65b124fda053a2
Franck <patouillardfranck3@gmail.com>
2019-12-09 17:34:10 +0100

    req api

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front';
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.http.get('https://localhost:8000/').subscribe(data => {
+      console.log(data)
+    });
+
+  }
 }
